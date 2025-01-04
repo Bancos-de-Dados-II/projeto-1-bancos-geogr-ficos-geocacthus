cb5083c6c683b0e55cc40e6d81a4644639f090d9
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-01-03 23:11:03 -0300

    adding the server's first structure

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const my_port = process.env.SERVER_PORT || 3000
+const  server = express();
+server.use(express.json());
+
+server.use('/', (req: Request, res: Response) => {
+    res.status(200).json({ message: "Server is runing!" });
+});
+
+server.listen(my_port, () => {
+    console.log(`Server is running in http://localhost:${my_port}`);
+})
