7715ce2f013578e8b1fcba3d30796dfa2364e3a8
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-01-04 09:21:31 -0300

    adding index routes

---
+++ server/src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+// Configure the router
+// configureNameEntidadeRoutes(router);
+// configureNameEntidadeRoutes(router);
+
+export default router
