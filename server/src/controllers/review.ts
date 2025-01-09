3b4d0adf91f24e2030169e8fc091a424598a7e07
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-01-09 15:29:00 -0300

    feat: typing DTO structure for review

---
+++ server/src/controllers/review.ts
@@ -0,0 +1,69 @@
+import { ModelStatic } from "sequelize";
+import { Review as ReviewModel } from "../models/review";
+
+interface IReviewDTO {
+    rating: number;
+    date: Date;
+}
+
+class Review {
+    private review: ModelStatic<ReviewModel>;
+
+    constructor(hoursModel: ModelStatic<ReviewModel>) {
+        this.review = hoursModel;
+    }
+
+    async createEvaluate(reviewDTO: IReviewDTO, touristLocationId: string, userId: string) {
+        const { rating, date } = reviewDTO;
+
+        if (!rating || !date) return { status: 400, message: "Missing required fields" };
+
+        try {
+            const newEvaluate = await this.review.create({ 
+                rating, 
+                date,
+                userId,
+                touristLocationId,
+            });
+
+            return { status: 201, message: "Evaluate created successfully", data: newEvaluate };
+        } catch (error) {
+            console.log(error);
+            return { status: 500, message: "Internal Server Error", error: (error as Error).message };
+        }
+    }
+
+    async getEvaluates(touristLocationId: string) {
+        try {
+            const evaluates = await this.review.findAll({
+                where: { touristLocationId },
+            });
+
+            return { status: 200, message: "Evaluates retrieved successfully", data: evaluates };
+        } catch (error) {
+            console.log(error);
+            return { status: 500, message: "Internal Server Error", error: (error as Error).message };
+        }
+    }
+
+    async deleteEvaluate(userId: string, touristLocationId: string, date: Date) {
+        try {
+            const review = await this.review.findOne({
+                where: {
+                    date: new Date(date),
+                    touristLocationID: touristLocationId,
+                    userID: userId,
+                },
+            });
+
+            if (!review) return { status: 404, message: "No evaluates found for this user" };
+
+            await review.destroy();
+
+            return { status: 200, message: "Evaluates deleted successfully" };
+        } catch (error) {
+            console.log(error);
+            return { status: 500, message: "Internal Server Error", error: (error as Error).message };
+        }
+    }
+}
