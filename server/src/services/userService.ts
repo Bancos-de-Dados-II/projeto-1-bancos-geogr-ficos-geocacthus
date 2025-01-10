d9d94434630cbdff3ee17aca0d3009e4a1ba411e
luizfernandoin <luizfernandoinfo100@gmail.com>
2025-01-10 10:50:36 -0300

    feat: criando função para pegar todos os users

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,10 +1,30 @@
 import { Model, ModelStatic } from "sequelize";
 import User from "../models/user";
+import HttpError from "../utils/error/httpError";
 
 class UserService {
     private userModel: ModelStatic<User>;
 
     constructor(userModel: ModelStatic<User>) {
         this.userModel = userModel;
-    }   
+    }
+
+    async getAllUsers() {
+        try {
+            const users = await this.userModel.findAll({
+                attributes: { exclude: ['password']},
+            });
+
+            return users;
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new HttpError("Não foi possível buscar usuários", 500, error);
+            }
+
+            throw new HttpError("Erro interno ao tentar buscar usuários.", 500);
+        }
+    }
 }
+
+
+export default UserService;
