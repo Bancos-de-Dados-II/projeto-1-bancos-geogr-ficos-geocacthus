21375154f07815d3788308a8a8709e6fb9767682
luizfernandoin <luizfernandoinfo100@gmail.com>
2025-01-09 21:20:06 -0300

    feat: adequando configurações

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import routes from './routes'
 import sequelize from './config/sequelize';
+import router from './routes/router';
+import HttpError from './utils/error/httpError';
+import errorMiddleware from './utils/middlewares/errorMiddleware';
 
 dotenv.config();
 
-const my_port = process.env.SERVER_PORT || 3000
-const  server = express();
+const PORT = process.env.SERVER_PORT || 3000
+const server = express();
 server.use(express.json());
+server.use(errorMiddleware);
 
-server.use('/', routes);
+server.use('/', router);
 
-server.listen(my_port, () => {
-    console.log(`Server is running in http://localhost:${my_port}\n`);
+server.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}\n`);
 })
 
 sequelize.sync().then(() => {
