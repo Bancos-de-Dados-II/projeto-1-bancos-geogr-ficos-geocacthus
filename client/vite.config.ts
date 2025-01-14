49173a0c983a716be5b93d6abe9b16f7328f323f
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-01-14 14:53:31 -0300

    refactor: structure refining

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: './src',
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: '../build',
+    rollupOptions: {
+      input: './src/index.html',
+    }
+  }
 })
