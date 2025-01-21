538bf425f0a7a33719c32796bce9a34eeb52bfd5
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-01-21 09:46:14 -0300

    feat: adding map and dimensions

--- client/src/apps/Turist/pages/Home/Home.tsx
+++ client/src/apps/Turist/pages/Home/Home.tsx
@@ -1,8 +1,70 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import { LatLngTuple } from "leaflet"
+import "./home.css"
+
+const touristLocations = [
+    {
+        id: 1,
+        name: "Ponto Turístico 1",
+        description: "Descrição do ponto turístico 1.",
+        position: [-7.135, -34.876] as LatLngTuple, // Coordenadas de exemplo
+    },
+    {
+        id: 2,
+        name: "Ponto Turístico 2",
+        description: "Descrição do ponto turístico 2.",
+        position: [-7.130, -34.870] as LatLngTuple, // Coordenadas de exemplo
+    },
+]
 
 function Home() {
-  return (
-    <div>Home Turist</div>
-  )
+    return (
+        <div className="home-turist-container">
+            <div className="content-header">
+                {/* campos de titulo, pesquisa e filtors de regioẽs */}
+                <div id="title" className="box-camp">
+                    <h2>Home Turist</h2>
+                </div>
+                <div id="search" className="box-camp">
+                    <h2>Search</h2>
+                </div>
+                <div id="filter" className="box-camp">
+                    <h2>Filter</h2>
+                </div>
+            </div>
+            <div className="content-main">
+                <div className="box-map">
+                    <MapContainer
+                        center={[-7.135, -34.876]} // Posição inicial do mapa
+                        zoom={13}
+                        style={{ height: "815px", width: "100%" }}
+                    >
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        />
+                        {touristLocations.map((location) => (
+                            <Marker key={location.id} position={location.position}>
+                                <Popup>
+                                    <strong>{location.name}</strong>
+                                    <br />
+                                    {location.description}
+                                </Popup>
+                            </Marker>
+                        ))}
+                    </MapContainer>
+                </div>
+                <div className="box-info">
+                    <div className="camp-info">
+                        <h3>info</h3>
+                    </div>
+                    <div className="camp-reviews">
+                        <h3>reviews</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
