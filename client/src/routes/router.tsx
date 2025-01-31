cd10ae243df6d9dc41e258ba70bdee46aed35a33
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-01-30 22:30:51 -0300

    refactor: standardization of route structure

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -4,36 +4,44 @@ import SignIn from "../screens/SignIn/SignIn";
 import SignUp from "../screens/SignUp/SignUp";
 import Home from "../screens/Home/Home";
 import CreateTouristLocation from "../screens/CreateTuristLocation/CreateTouristLocation";
-import Profile  from "../screens/Profile/Profile";
+import Profile from "../screens/Profile/Profile";
 
 import ProtectedRoute from "../components/ProtectedRoute";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route index element={<Navigate to="/signin" />} />
-
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-
+            <Route index
+                element={
+                    <Navigate to="/signin" />
+                }
+            />
             <Route
-                path="/home" 
+                path="/signin"
+                element={<SignIn />}
+            />
+            <Route
+                path="/signup"
+                element={<SignUp />}
+            />
+            <Route
+                path="/home"
                 element={
                     <ProtectedRoute>
-                        <Home />    
+                        <Home />
                     </ProtectedRoute>
                 }
             />
-            <Route 
-                path="/create/tourist-place" 
+            <Route
+                path="/create/tourist-place"
                 element={
                     <ProtectedRoute>
                         <CreateTouristLocation />
                     </ProtectedRoute>
                 }
             />
-            <Route 
-                path="/profile" 
+            <Route
+                path="/profile"
                 element={
                     <ProtectedRoute>
                         <Profile />
